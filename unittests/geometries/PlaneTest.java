684a0c2c5ef36964d1d9619197e9c2e99d98c474
sigmaPigeon <benyitzhaky@yamtal.co.il>
2025-05-06 23:17:11 +0300

    mid development

---
+++ unittests/geometries/PlaneTest.java
@@ -0,0 +1,11 @@
+package geometries;
+
+import static org.junit.jupiter.api.Assertions.*;
+import geometries.Plane;
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
+
+class PlaneTest {
+
+}
